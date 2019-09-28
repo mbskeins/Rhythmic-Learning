f5050cd432142705082db5854c77c4cf8c466897
Jacob Lebowitz <augmentedmode@Jacobs-MacBook-Pro-5.local>
2019-09-28 02:03:31 -0400

    text preprocessing complete

--- preprocess_text.py
+++ preprocess_text.py
@@ -3,18 +3,41 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 
 import wikipedia
 import string
-import re
+import pyphen
 
-p = wikipedia.page('Cat')
+# Get page on a specific topic from wikipedia
+p = wikipedia.page('chase bank')
 
 page_url = p.url
 page_title = p.title
 page_content = p.content # Content of page
 
-# List containing the first 15 sentences of the page (page summary)
-sentences = sent_tokenize(page_content)[:15]
+def clean_page_content(page_content):
+    # List containing the first 15 sentences of the page (page summary)
+    sentences = sent_tokenize(page_content)[:15]
+    # Removed punctuation from all sentences
+    cleaned_sentences = [sent.translate(str.maketrans('', '', string.punctuation)) for sent in sentences]
+    # Lower case all letters in sentences
+    cleaned_sentences = [sent.lower() for sent in cleaned_sentences]
 
-# Removed punctuation from all sentences
-cleaned_sentences = [sent.translate(str.maketrans('', '', string.punctuation)) for sent in sentences]
+    return cleaned_sentences
 
-print(cleaned_sentences)
+def sylabize_all_words(cleaned_sentences):
+    dic = pyphen.Pyphen(lang='nl_NL')
+
+    all_sentences = []
+    for sent in cleaned_sentences:
+        sentence = []
+        words = sent.split()
+        for word in words:
+            sentence.append(dic.inserted(word))
+        all_sentences.append(sentence)
+
+    return all_sentences
+
+
+cleaned_sentences = clean_page_content(page_content)
+
+hyphened_sentences = sylabize_all_words(cleaned_sentences)
+
+print(hyphened_sentences)
