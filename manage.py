316fbe126a60421082c9a6975cd4fba2b4988a7a
Matt Skeins <mbskeins@Matts-MacBook-Air.local>
2019-09-29 02:34:14 -0400

    manager change

--- manage.py
+++ manage.py
@@ -8,8 +8,6 @@ from app import app, db
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-manager.add_command('db', MigrateCommand)
-
 
 if __name__ == '__main__':
     app.run()
