8ad9a6b30396a720052a85d038f1c1b397b1b60b
Matt Skeins <mbskeins@Matts-MacBook-Air.local>
2019-09-29 01:29:47 -0400

    deleted front end

--- Rhythmic_Learning/angular_app/src/app/models/AdLib.ts
+++
@@ -1,9 +0,0 @@
-export class AdLib{
-    public audio: any;
-    public number: number;
-
-    constructor(audio: any, number: number){
-        this.audio = audio;
-        this.number = number;
-    }
-}
--- Rhythmic_Learning/angular_app/src/app/models/TtsInstance.ts
+++
@@ -1,9 +0,0 @@
-export class TtsInstance {
-    public text: string;
-    public delay: number;
-
-    constructor(text: string, delay: number){
-        this.text = text;
-        this.delay = delay;
-    }
-}
