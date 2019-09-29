eb3678668d9d469797be77e02a72062b74e8c16a
Matt Skeins <mbskeins@Matts-MacBook-Air.local>
2019-09-29 02:33:11 -0400

    manager change

--- manage.py
+++ manage.py
@@ -1,17 +1,15 @@
 import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask import Flask
 
 from app import app, db
 
-
+app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-
-migrate = Migrate(app, db)
-manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
