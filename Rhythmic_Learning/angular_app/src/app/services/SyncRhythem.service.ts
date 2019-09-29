ece107df131537d25cea844710eac033633a185c
Jared <jaredfellowscs@gmail.com>
2019-09-29 01:53:49 -0400

    sync attempt on sentences one

--- Rhythmic_Learning/angular_app/src/app/services/SyncRhythem.service.ts
+++ Rhythmic_Learning/angular_app/src/app/services/SyncRhythem.service.ts
@@ -10,6 +10,8 @@ export class SyncRhythemService {
   private music: Howl;
   private isBeatPlaying: boolean;
 
+  private sentences: string[];
+  currentSentenceIndex = 0;
 constructor() { 
   console.log("SyncRhythem attached");
   this.music = new Howl({
@@ -21,6 +23,54 @@ constructor() {
   });
 }
 
+  public startTtsWithSentences(sentences: string[], voiceURI: string){
+    this.sentences = sentences;
+    this.currentSentenceIndex = 0;
+
+    this.music.play();
+    this.isBeatPlaying = true;
+    
+    const sound = new Speech();
+    sound.init({
+      'volume': 1,
+      'lang': 'en-GB',
+      'rate': 1.05,
+      'pitch': 1,
+      'voice':voiceURI,
+      'splitSentences': true
+    });
+    this.playNextSentence(sound);
+  }
+
+  private playNextSentence(sound: Speech){
+    if(this.currentSentenceIndex >= this.sentences.length) return;
+    var currentSentence = this.sentences[this.currentSentenceIndex];
+
+    var startTimeInMili = new Date().getTime();
+    var quarterNote = 0.35;
+    var quarterNoteMili = quarterNote * 1000;
+    var measureTimeMili = quarterNoteMili * 4;
+
+      sound.speak({
+        text: currentSentence,
+        queue: false,
+        listeners: {
+          onend: () => {
+            var endTimeInMili = new Date().getTime();
+            var differenceInMili = endTimeInMili - startTimeInMili;
+            var numberOfMeasuresThatPassed = differenceInMili / measureTimeMili;
+            var percentageOfMeasureToPad = Math.ceil(numberOfMeasuresThatPassed) - numberOfMeasuresThatPassed;
+            var delayInMili = percentageOfMeasureToPad * measureTimeMili;
+            setTimeout(() => {
+              this.playNextSentence(sound);
+            }, delayInMili);
+          }
+        }
+      });
+
+      ++this.currentSentenceIndex;
+  }
+
   public startTts(ttsInstance: TtsInstance, voiceURI: string){
     const sound = new Speech();
     sound.init({
@@ -31,6 +81,7 @@ constructor() {
       'voice':voiceURI,
       'splitSentences': true
     });
+    var startTimeInMili = new Date().getTime();
     this.music.play();
     this.isBeatPlaying = true;
     setTimeout(() => {
@@ -38,8 +89,7 @@ constructor() {
         text: ttsInstance.text,
         queue: false,
         listeners: {
-          onstart: () => {
-            //start ad libs
+          onend: () => {
           }
         }
       });
